3138228b194fbdf37a13233a0a7119603ef2b5a5
hzp <791402133@qq.com>
2021-08-18 18:49:14 +0800

    完成了主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -3,6 +3,22 @@
 
 $.ajaxPrefilter(function (options){
     //这是获取的url
-    console.log(options.url)
+    // console.log(options.url)
     options.url = 'http://api-breakingnews-web.itheima.net' + options.url
+    if (options.url.indexOf('/my/') !== -1){
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+    //不论成功还是失败,最终都会调用complete回调函数
+    options.complete = function (res){
+        console.log(res)
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！'){
+            //1.强制清空token
+            localStorage.removeItem('token')
+            //2.强制跳转到登录页面
+            location.href = 'login.html'
+        }
+    }
+
 })
